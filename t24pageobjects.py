67fbf60bbeed633f7ef62ce78dabf8599e0beddc
Ivan Mitev <immitev@gmail.com>
2015-03-12 22:50:14 +0200

    LoginPage should be ready

---
+++ t24pageobjects.py
@@ -0,0 +1,85 @@
+from robotpageobjects import Page, robot_alias
+from robot.utils import asserts
+
+class T24LoginPage(Page):
+    """ Models the T24 login page"""
+
+    # Allows us to call by proper name
+    name = "T24Login"
+
+    # This page is found at baseurl
+    uri = "/BrowserWeb"
+
+    # Inheritable dictionary mapping human-readable names to Selenium2Library locators. 
+    # You can then pass in the keys to Selenium2Library actions instead of the locator strings.
+    selectors = {
+        "user input": "xpath=.//*[@id='signOnName']",
+        "password input": "xpath=.//*[@id='password']",
+        "login button": "css=#sign-in",
+    }
+
+    # Use robot_alias and the "__name__" token to customize where to insert the optional page object name when calling this keyword. 
+    @robot_alias("type_in__name__username_box")
+    def _type_username(self, txt):
+        self.input_text("user input", txt)
+
+        # We always return something from a page object, even if it's the same page object instance we are currently on.
+        return self
+
+    # Use robot_alias and the "__name__" token to customize where to insert the optional page object name when calling this keyword. 
+    @robot_alias("type_in__name__password_box")
+    def _type_password(self, txt):
+        self.input_text("password input", txt)
+
+        # We always return something from a page object, even if it's the same page object instance we are currently on.
+        return self
+
+    # Use robot_alias and the "__name__" token to customize where to insert the optional page object name when calling this keyword. 
+    @robot_alias("clicks__name__login_button")
+    def _click_login(self):
+        self.click_element("login button")
+
+        # We always return something from a page object, even if it's the same page object instance we are currently on.
+        return T24HomePage()
+
+    @robot_alias("enter_T24_credentials")
+    def enter_T24_credentials(self, username, password):
+        self._type_username (username)
+        self._type_password(password)
+        return self._click_login();
+
+
+class T24HomePage(Page):
+    """ Models the T24 home page """
+
+    # Allows us to call by proper name
+    name = "T24Home"
+
+    # This page is found at baseurl + "/BrowserWeb"
+    uri = "/BrowserWeb"
+
+    # Inheritable dictionary mapping human-readable names to Selenium2Library locators. 
+    # You can then pass in the keys to Selenium2Library actions instead of the locator strings.
+    selectors = {
+        "banner frame": "xpath=//frame[contains(@id,'banner')]",
+        "command line": "css=input[name='commandValue']",
+    }
+
+    # Use robot_alias and the "__name__" token to customize
+    # where to insert the optional page object name
+    # when calling this keyword. Without the "__name__"
+    # token this method would map to either "Type In Search Box",
+    # or "Type In Search Box Pubmed". Using "__name__" we can call
+    # "Type in Pubmed Search Box  foo".
+    @robot_alias("enter_t24_command")
+    def enter_t24_command(self, text):
+        self.select_frame(selectors["banner frame"])
+        self.wait_until_page_contains_element(selectors["command line"])
+        self.input_text(selectors["command line"], text+"\n")
+
+        # We always return something from a page object, 
+        # even if it's the same page object instance we are
+        # currently on.
+        return self
+
+
---
+++ test_t24pageobjects.py
@@ -0,0 +1,23 @@
+from t24pageobjects import T24LoginPage,T24HomePage
+import os
+import unittest
+
+
+class T24LoginPageTestCase(unittest.TestCase):
+
+    def setUp(self):
+        os.environ["PO_BASEURL"] = "http://192.168.131.132:9095"
+        os.environ["PO_BROWSER"] = "firefox"
+        
+        self.loginpage = T24LoginPage()
+        self.loginpage.open()
+
+    def test_login(self):
+        page = self.loginpage.enter_T24_credentials("INPUTT","123456")
+        page.enter_t24_command(page,"AAA")
+
+    def tearDown(self):
+        self.loginpage.close()
+
+if __name__ == "__main__":
+    unittest.main()
