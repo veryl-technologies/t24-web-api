7bc7b11358d9d67da7c84961e7057c69c6d05eec
Ivan Mitev <immitev@gmail.com>
2015-07-08 15:27:23 +0300

    Experiments with T24 enquiries

--- t24pageobjects.py
+++ t24pageobjects.py
@@ -48,7 +48,6 @@ class T24LoginPage(Page):
         self._type_password(password)
         return self._click_login()
 
-
 class T24HomePage(Page):
     """ Models the T24 home page """
 
@@ -58,23 +57,58 @@ class T24HomePage(Page):
     # This page is found at baseurl + "/BrowserWeb"
     uri = "/BrowserWeb"
 
-    # Inheritable dictionary mapping human-readable names to Selenium2Library locators. 
+    # Inheritable dictionary mapping human-readable names to Selenium2Library locators.
     # You can then pass in the keys to Selenium2Library actions instead of the locator strings.
     selectors = {
         "banner frame": "xpath=//frame[contains(@id,'banner')]",
         "command line": "css=input[name='commandValue']",
     }
 
-    # Enter a T24 API command
+    # Enter a T24 command
     @robot_alias("enter_t24_command")
     def enter_t24_command(self, text):
         self.select_frame(self.selectors["banner frame"])
         self.wait_until_page_contains_element(self.selectors["command line"])
         self.input_text(self.selectors["command line"], text+"\n")
 
-        # We always return something from a page object, 
+        # We always return something from a page object,
+        # even if it's the same page object instance we are currently on.
+        return self
+
+    # Enter a T24 enquiry API command
+    @robot_alias("enter_t24_enquiry")
+    def open_t24_enquiry(self, text):
+        self.enter_t24_command(text)
+
+        return T24EnquiryStartPage()
+
+class T24EnquiryStartPage(Page):
+    """ Models the T24 Enquiry Start Page"""
+
+    # Allows us to call by proper name
+    name = "T24Enquiry"
+
+    # Probably not necessary
+    uri = "/BrowserWeb/servlet/BrowserServlet#1"
+
+    # Inheritable dictionary mapping human-readable names to Selenium2Library locators.
+    # You can then pass in the keys to Selenium2Library actions instead of the locator strings.
+    selectors = {
+        "clear selection link": "xpath=.//a[contains(normalize-space(.), 'Clear Selection')]",
+        "find button": "xpath=.//a[@title='Run Selection']",
+    }
+
+    # Runs a T24 enquiry
+    @robot_alias("run_enquiry")
+    def run_enquiry(self, fieldName, fieldValue):
+        self.wait_until_page_contains_element(self.selectors["clear selection link"])
+        self.wait_until_page_contains_element(self.selectors["find button"])
+
+        self.click_element(self.selectors["clear selection link"])
+        self.click_element(self.selectors["find button"])
+
+        # We always return something from a page object,
         # even if it's the same page object instance we are
         # currently on.
         return self
-
 
--- test_t24pageobjects.py
+++ test_t24pageobjects.py
@@ -1,20 +1,22 @@
 from t24pageobjects import T24LoginPage,T24HomePage
 import os
 import unittest
+import time
 
 
 class T24LoginPageTestCase(unittest.TestCase):
 
     def setUp(self):
-        os.environ["PO_BASEURL"] = "http://192.168.131.132:9095"
+        os.environ["PO_BASEURL"] = "http://7.117.75.57:9095"
         os.environ["PO_BROWSER"] = "firefox"
         
         self.loginpage = T24LoginPage()
         self.loginpage.open()
 
-    def test_login(self):
-        page = self.loginpage.enter_T24_credentials("INPUTT","123456")
-        page.enter_t24_command("CUSTOMER I F3")
+    def test_enquiry(self):
+        homePage = self.loginpage.enter_T24_credentials("INPUTT","123456")
+        homePage.enter_t24_command("ENQ %CUSTOMER")
+        time.sleep(3)
 
     def tearDown(self):
         self.loginpage.close()
