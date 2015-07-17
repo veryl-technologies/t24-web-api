e5fd9493bd6e0a4d43f14983487a3d5b872f0c9c
Ivan Mitev <immitev@gmail.com>
2015-07-17 08:49:42 +0300

    Added BAT file to build XML doc files for the page objects

---
+++ rfDocGen.bat
@@ -0,0 +1,6 @@
+python -m robot.libdoc t24pageobjects.T24LoginPage T24LoginPage.xml
+python -m robot.libdoc t24pageobjects.T24HomePage T24HomePage.xml
+python -m robot.libdoc t24pageobjects.T24EnquiryStartPage T24EnquiryStartPage.xml
+python -m robot.libdoc t24pageobjects.T24EnquiryResultPage T24EnquiryResultPage.xml
+python -m robot.libdoc t24pageobjects.T24RecordSeePage T24RecordSeePage.xml
+python -m robot.libdoc t24pageobjects.T24RecordSeePage T24RecordSeePage.xml
