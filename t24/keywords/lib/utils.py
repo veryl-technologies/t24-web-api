f83804c3701a6ac601d5f7748c0cefcc9aff6003
Ivan Mitev <immitev@gmail.com>
2016-02-25 17:05:10 +0200

    get_unique_new_customer_mnemonic() - actual implementation based on current time

--- t24/keywords/lib/utils.py
+++ t24/keywords/lib/utils.py
@@ -7,10 +7,27 @@ class utils:
  
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
     __version__ = '0.1'
- 
+
+    @staticmethod
+    def num_to_short_alphanumeric(i):
+        chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXY'
+        length = len(chars)
+
+        result = ''
+        while i:
+            result = chars[i % length] + result
+            i = i // length
+        if not result:
+            result = chars[0]
+        return result
+
     def get_unique_new_customer_mnemonic(self):
-    	return 'XYZ12345'
-    
+        base = datetime.datetime(2016, 2, 1, 00, 00)
+        secondsPassed = (datetime.datetime.now() - base).total_seconds()
+        code = self.num_to_short_alphanumeric(secondsPassed)
+        result = code.rjust(8, 'Z')
+        return result
+
     def get_unique_date_string(self):
         now = datetime.datetime.now()
         return now.strftime("%m%d%H%M%S")
