9bac1845c6f08f6b81e9e86155b46dc9ada86dff
Ivan Mitev <immitev@gmail.com>
2016-05-27 21:58:02 +0300

    Cleanup VariablesExporter - simpler code and removed old commented out code

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ class Config:
         else:
             return version
 
+
 class BuiltinFunctions:
  
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
@@ -42,14 +43,17 @@ class BuiltinFunctions:
 
 
 class VariablesExporter:
+
     def add(self, name, value):
         output_file = BuiltIn().get_variable_value("${EXPORT_GLOBAL_VARS_TO_FILE}")
-        if output_file and len(output_file) > 0:
-            output_file_created = BuiltIn().get_variable_value("${EXPORT_GLOBAL_VARS_TO_FILE_CREATED}")
-            if not output_file_created or len(output_file_created) <= 0:
-                self._create_output_file(output_file)
-                BuiltIn().set_global_variable("${EXPORT_GLOBAL_VARS_TO_FILE_CREATED}", output_file)
-                output_file_created = output_file
+        if not output_file:
+            raise Exception('EXPORT_GLOBAL_VARS_TO_FILE not defined.')
+
+        # within the RF execution session, make sure that the previous variables file is backed up and a new one started
+        output_file_created = BuiltIn().get_variable_value("${EXPORT_GLOBAL_VARS_TO_FILE_CREATED}")
+        if not output_file_created:
+            self._backup_previous_and_create_new_file(output_file)
+            BuiltIn().set_global_variable("${EXPORT_GLOBAL_VARS_TO_FILE_CREATED}", output_file)  # just sth non-empty
 
         self._add_variable(output_file, name, value)
 
@@ -58,14 +62,6 @@ class VariablesExporter:
         with open(output_file, "rb+") as f:
             f.seek(0, os.SEEK_END)
             f.write(name + " = " + VariablesExporter._format_value(value) + "\r\n")
-    """ Old format using python method return value
-        with open(output_file, "rb+") as f:
-            # remove the last character '}'
-            f.seek(-1, os.SEEK_END)
-            f.truncate()
-            f.write("\r\n")
-            f.write("            \"" + name + "\": " + VariablesExporter._format_value(value) + ",}")
-    """
 
     @staticmethod
     def _format_value(value):
@@ -75,18 +71,11 @@ class VariablesExporter:
             return "\"" + value + "\""
 
     @staticmethod
-    def _create_output_file(outputFile):
-        VariablesExporter._assure_directory_exists(outputFile)
-
-        if os.path.isfile(outputFile):
-            os.rename(outputFile, VariablesExporter._get_history_file_name(outputFile))
-
-        """ Old format using python method return value
-        with open(outputFile, "w+") as f:
-            f.write("def get_variables():\r\n")
-            f.write("    return {}")
-        """
-        with open(outputFile, "w+") as f:
+    def _backup_previous_and_create_new_file(path):
+        VariablesExporter._assure_directory_exists(path)
+        if os.path.isfile(path):
+            os.rename(path, VariablesExporter._get_history_file_name(path))
+        with open(path, "w+") as f:
             f.write("")
 
     @staticmethod
@@ -96,5 +85,5 @@ class VariablesExporter:
             os.makedirs(d)
 
     @staticmethod
-    def _get_history_file_name(fileName):
-        return fileName + time.strftime('%Y-%m-%d-%H-%M-%S', time.gmtime(os.path.getmtime(fileName)))
+    def _get_history_file_name(path):
+        return path + time.strftime('%Y-%m-%d-%H-%M-%S', time.gmtime(os.path.getmtime(path)))
